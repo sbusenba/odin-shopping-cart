f5b0360e212201909cac46bf5fea6ada4992279f
Steven Busenbarrick <sbusenba@gmail.com>
2022-10-11 16:02:59 -0700

    Create productView class for displaying items

--- src/components/ProductView.js
+++ src/components/ProductView.js
@@ -1,6 +1,26 @@
+import { useState } from 'react'
+import '../styles/productView.css'
 function ProductView (props){
+    let [quantity,setQuantity]= useState(0)
+
+    let click = ()=>{
+        let itemName = props.product.name;
+        let itemPrice = props.product.price
+        props.addToCart({name:itemName,price:itemPrice,quantity})
+        setQuantity(0)
+    }
+    let qtyChange = (e)=>{
+        setQuantity(e.target.value)
+    }
+
     return (<div>
-        {props.product}
+        <img src={props.product.image}></img>
+        <div>{props.product.name}</div>
+        <div>${props.product.price}</div>
+        <label htmlFor='qtyInput'> Quantity:
+            <input type='text' onChange={qtyChange}/>
+        </label>
+        <button onClick={click}>Add to Cart</button>
     </div>)
 }
 export default ProductView;
