9adf47082074af8f7834fc4c03a3bdf3010bd1a7
Steven Busenbarrick <sbusenba@gmail.com>
2022-10-11 16:42:33 -0700

    build initial cart ui

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,16 @@
+import  {useOutletContext} from 'react-router-dom'
+
 function Cart (){
+
+    const [products,cartList,addToCart] = useOutletContext()
+
     return (<div>
-        here is the cart!
+        Your Cart:
+            <ul>
+        {cartList.map((item,index)=>{return <li key={index}> 
+            {`${item.name}s, ${item.quantity} at $${item.price}`}
+        </li>})}
+        </ul>
     </div>)
 }
 export default Cart;
