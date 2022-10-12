b56b05becf4e83b511eb731acc9ad2347a9481cf
Steven Busenbarrick <sbusenba@gmail.com>
2022-10-12 10:55:43 -0700

    Add logic to sum cart total.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,14 +3,17 @@ import  {useOutletContext} from 'react-router-dom'
 function Cart (){
 
     const [products,cartList,addToCart] = useOutletContext()
-
+    let total = 0;
     return (<div>
         Your Cart:
             <ul>
-        {cartList.map((item,index)=>{return <li key={index}> 
-            {`${item.name}s, ${item.quantity} at $${item.price}`}
+        {cartList.map((item,index)=>{
+            total += item.price*item.quantity;
+            return <li key={index}> 
+            {`${item.name}s, ${item.quantity} at $${item.price} = $${item.price*item.quantity}`}
         </li>})}
         </ul>
+        <div>{`Total: $${total}`}</div>
     </div>)
 }
 export default Cart;
