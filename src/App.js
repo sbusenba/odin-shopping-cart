bdf66fa1b1703d6e2b012551f75a9a0a7405049f
Steven Busenbarrick <sbusenba@gmail.com>
2022-10-14 06:42:29 -0700

    Update CSS to add semitransparent bcg for title.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import {Link} from 'react-router-dom'
 import './App.css';
 
 function App() {
+  const linkStyle = {
+    margin: "1rem",
+    textDecoration: "none",
+    color: "#f5deb3",
+  };
   return (
     <div className="App">
-      <div> Welcome to my Produce <Link to="shop">Store</Link></div>
+      <div className='titleBcg'>
+        <div className='titleContent'>
+        <h1> Welcome to my Produce Cart,</h1>
+        <p> Click <Link to="shop" style={linkStyle}>here</Link> to shop.</p>
+        </div>
+      </div>
     </div>
   );
 }
