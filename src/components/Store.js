38f9f0caae1ed4873a4fa3e7ff31cd64a5f4f9fa
Steven Busenbarrick <sbusenba@gmail.com>
2022-10-17 10:51:45 -0700

    add logic to check for duplicate item in addtocart

--- src/components/Store.js
+++ src/components/Store.js
@@ -19,8 +19,18 @@ function Store (){
     const addToCart = ({name,price,quantity})=>{
         quantity = parseInt(quantity)
         if (quantity>0){
+        let newCartList = cartList;
+        let duplicateItem = false;
+        newCartList.forEach((cartItem)=>{
+            if (cartItem.name === name){
+                cartItem.quantity +=quantity;
+                duplicateItem= true;
+                setCartList([...newCartList])
+            }
+        }) 
+        if (!duplicateItem){
             setCartList([{name,price,quantity},...cartList])
-        }
+        }}
     }
     
     const updateCart=(updatedCart)=>{
